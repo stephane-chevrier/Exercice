3681ef82d25b3cb411fd1b1ed8b364e9b6dedb4b
stephane.chevrier <stephane.chevrier@le-campus-numerique.fr>
2022-12-09 10:59:48 +0100

    add gitignore + modif main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
-
+    // Affichage liste de nombres entiers avec boucle while
     static void enumerate_while(int nombre) {
         int i = 0;
         while (i <= nombre) {
             System.out.println(i++);
         }
     }
-
-    static void enumerate_for(int nombre2) {
+    // Affichage liste de nombres entiers avec boucle for
+    // static void enumerate_for(int nombre2) {
         for (int i=0;i<=nombre2;i++) {
             System.out.println(i);
         }
     }
-
+    // Affichage liste de nombres entiers pairs avec boucle while
     static void enumerate_even(int nombre3) {
         for (int i = 0; i <= nombre3; i++) {
             if (i % 2 == 0) {
@@ -20,11 +20,11 @@ public class Main {
             }
         }
     }
-
-    public static void main(String[] args) {
+        // Programme principal
+        public static void main(String[] args) {
+        // Appel des méthodes
         enumerate_while(100);
         enumerate_for(100);
         enumerate_even (100);
-
     }
 }
