7691d5947dcd563056d3c78e0b0b98e4b7870718
stephane.chevrier <stephane.chevrier@le-campus-numerique.fr>
2022-12-09 14:10:42 +0100

    Exercice2

--- src/Exercice2.java
+++ src/Exercice2.java
@@ -1,7 +1,13 @@
 
 public class Exercice2 {
-
-    void int plusPetitcarre (int nombre) {
-        double
+    public int plusGrandCarreInf (int nombre) {
+        int calcul;
+        calcul = (int)Math.floor(Math.sqrt(nombre)) ;
+        return calcul * calcul;
+    }
+    public int plusPetitCarreSup (int nombre) {
+        int calcul;
+        calcul = (int)Math.ceil(Math.sqrt(nombre));
+        return calcul * calcul;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 /*
-        Exercice1
+        Exercice 1
         Exercice1 exo1 = new Exercice1();
         exo1.enumerate_while(100);
         exo1.enumerate_for(100);
         exo1.enumerate_even(100);
 */
-
+        /*
+ Exercice 2
+        Exercice2 exo2 = new Exercice2();
+        Scanner clavier = new Scanner(System.in);
+        int entree = clavier.nextInt();
+        System.out.println("Plus grand carré inférieur : "+exo2.plusGrandCarreInf(entree));
+        System.out.println("Plus petit carré supérieur : "+exo2.plusPetitCarreSup(entree));
+*/
     }
 }
